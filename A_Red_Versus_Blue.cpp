45827ce37b4ca8238daa1a16dff9aff2ab958549
Parthmudgal15105 <parthmudgal15105@gmail.com>
2024-10-30 00:12:50 +0530

    1000s part2

---
+++ A_Red_Versus_Blue.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// Add the necessary include paths to the compiler flags
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+// Types of declarations
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+
+// Constants
+const ll MOD = 1e9 + 7;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// Odd Even
+bool odd(ll num) { return ((num & 1) == 1); }
+bool even(ll num) { return ((num & 1) == 0); }
+
+// Prime check
+bool isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+// GCD & LCM
+long long gcd(long long a, long long b) {
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b) {
+    return (a / gcd(a, b)) * b; 
+}
+
+// Square Root
+long long sqrt(long long x) {
+    long long s = 0, e = 2e9, res = s;
+    while (s <= e) {
+        long long m = (s + e) / 2;
+        if (m * m <= x) res = m, s = m + 1;
+        else e = m - 1;
+    }
+    return res;
+}
+
+void solve(){
+	
+	int n, r, b;
+	cin>>n>>r>>b;
+ 
+	string ans;
+	int nreds = r/(b+1);
+	int rem = r%(b+1);
+	//put reds put a b , put reds put a b
+	for(int i=0; i<b; i++){
+		for(int j=0; j<nreds; j++)
+			ans.push_back('R');
+		if(rem){
+			ans.push_back('R');
+			rem--;
+		}
+		ans.push_back('B');
+	}
+	//rem = 0;
+	for(int i=0; i<nreds; i++)
+		ans.push_back('R');
+ 
+	cout<<ans<<nl;
+ 
+	
+ 
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    
+    return 0;
+}
---
+++ D_Black_and_White_Stripe.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// Add the necessary include paths to the compiler flags
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+// Types of declarations
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+
+// Constants
+const ll MOD = 1e9 + 7;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// Odd Even
+bool odd(ll num) { return ((num & 1) == 1); }
+bool even(ll num) { return ((num & 1) == 0); }
+
+// Prime check
+bool isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+// GCD & LCM
+long long gcd(long long a, long long b) {
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+long long lcm(long long a, long long b) {
+    return (a / gcd(a, b)) * b; 
+}
+
+// Square Root
+long long sqrt(long long x) {
+    long long s = 0, e = 2e9, res = s;
+    while (s <= e) {
+        long long m = (s + e) / 2;
+        if (m * m <= x) res = m, s = m + 1;
+        else e = m - 1;
+    }
+    return res;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+
+    int ans = INT_MAX;
+    
+    // Calculate the initial window's tempAns
+    int tempAns = 0;
+    for (int i = 0; i < k; i++) {
+        if (s[i] == 'W') {
+            tempAns++;
+        }
+    }
+    
+    ans = min(ans, tempAns); // First window
+
+    // Slide the window
+    for (int i = k; i < n; i++) {
+        // Remove the element going out of the window
+        if (s[i - k] == 'W') {
+            tempAns--;
+        }
+        // Add the new element into the window
+        if (s[i] == 'W') {
+            tempAns++;
+        }
+
+        ans = min(ans, tempAns); // Update minimum after each slide
+    }
+
+    cout << ans << endl; // Use endl instead of nl
+}
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    
+    return 0;
+}
